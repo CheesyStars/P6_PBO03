f5f0bb2438543612f8a6d0ee1cfc8d17ae273515
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-04-17 10:51:10 +0700

    Praktikum 6 Abstract Class - Interface - Inner Class
    Percobaan 4 : Implementasi Multiple Inheritance

---
+++ src/Percobaan_04/FirstInterface.java
@@ -0,0 +1,11 @@
+package Percobaan_04;
+
+//Membuat class interface
+public interface FirstInterface {
+    //Membuat variabel
+    int NUMBER = 13;
+
+    //Membuat method
+    void printHello();
+    int doubleNumberA();
+}
---
+++ src/Percobaan_04/SecondInterface.java
@@ -0,0 +1,11 @@
+package Percobaan_04;
+
+//Membuat class interface
+public interface SecondInterface {
+    //Membuat variabel
+    int NUMBER = 11;
+
+    //Membuat method
+    void printHello();
+    int doubleNumberB();
+}
